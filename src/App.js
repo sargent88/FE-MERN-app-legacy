d19a59835e99c85d9eef60ee86bc999777d75e5e
Steen G Sargent <ssargent@nerdunited.com>
2025-04-09 15:47:31 -0600

    fix: add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Navigate,
@@ -6,14 +6,18 @@ import {
   Routes,
 } from "react-router-dom";
 
-import { NewPlace, UpdatePlace, UserPlaces } from "./places/pages";
-import Login from "./users/pages/Login";
-import Signup from "./users/pages/Signup";
-import Users from "./users/pages/Users";
 import { MainNavigation } from "./shared/components/Navigation";
 import { AuthenticationContext } from "./shared/context/authenticationContext";
 import { useAuthentication } from "./shared/hooks/authHook";
+import { LoadingSpinner } from "./shared/components/UIElements";
 import "./App.css";
+
+const Users = React.lazy(() => import("./users/pages/Users"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const Login = React.lazy(() => import("./users/pages/Login"));
+const Signup = React.lazy(() => import("./users/pages/Signup"));
 
 function App() {
   const { login, logout, token, userId } = useAuthentication();
@@ -32,25 +36,33 @@ function App() {
 
 function MainRoutes({ token }) {
   return (
-    <main>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/:uid/places" element={<UserPlaces />} />
-        {token ? (
-          <>
-            <Route path="/places/new" element={<NewPlace />} />
-            <Route path="/places/:pid" element={<UpdatePlace />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </>
-        ) : (
-          <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </>
-        )}
-      </Routes>
-    </main>
+    <Suspense
+      fallback={
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/:uid/places" element={<UserPlaces />} />
+          {token ? (
+            <>
+              <Route path="/places/new" element={<NewPlace />} />
+              <Route path="/places/:pid" element={<UpdatePlace />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </>
+          ) : (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </>
+          )}
+        </Routes>
+      </main>
+    </Suspense>
   );
 }
 
